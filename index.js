5263b3a49d9bb29e41346c3b23ff6bdef027b1ef
Dan Saunders <admin@awsumchan.org>
2013-10-12 22:00:43 -0400

    Version 0.3.0
    Changelog:
    - Removed core usage of 'routes' module
    - Removed core usage of 'underscore.string' module
    - fdongles now extends irc.Client instead of events.EventEmitter
    - Removed default commands
    - Removed now unnecessary dependencies
    - Bumped version (duh)
    - Overall, removed a bunch of shit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var EventEmitter, Router, colors, fs, irc, pack, registerDefaultRoutes, _,
+var colors, fs, irc, _,
   __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   __hasProp = {}.hasOwnProperty,
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
@@ -10,81 +10,45 @@ irc = require('irc');
 
 colors = require('colors');
 
-Router = require('routes');
-
-EventEmitter = require('events').EventEmitter;
-
 _ = require('underscore');
-
-_.str = require('underscore.string');
-
-pack = JSON.parse(fs.readFileSync("" + __dirname + "/package.json"));
-
-registerDefaultRoutes = function(bot) {
-  bot.route('!info', function(res) {
-    var chan;
-    return bot.say(res.channel, "h :) v" + pack.version + "\nCurrent channels: " + (((function() {
-      var _results;
-      _results = [];
-      for (chan in bot.channels) {
-        _results.push(chan);
-      }
-      return _results;
-    })()).join(', ')) + "\n" + pack.repository.url);
-  });
-  bot.route('!auth :username :password', bot.authenticate, function(res) {
-    return bot.say(res.channel, "You are now authed. Hi " + (_.str.capitalize(res.user.username)) + "!");
-  });
-  bot.route('!join :channel', bot.requiresUser, function(res) {
-    return bot.join(res.params.channel);
-  });
-  bot.route('!join :channel :password', bot.requiresUser, function(res) {
-    return bot.join(res.params.channel + ' ' + res.params.password);
-  });
-  bot.route('!part :channel', bot.requiresUser, function(res) {
-    return bot.part(res.params.channel);
-  });
-  bot.route('!load :module', bot.requiresUser, function(res) {
-    return bot.load(res.params.module, function(err) {
-      if (err != null) {
-        return bot.say(res.channel, err);
-      }
-      return bot.say(res.channel, "Module '" + res.params.module + "' loaded!");
-    });
-  });
-  return bot.route('!stop :module', bot.requiresUser, function(res) {
-    return bot.stop(res.params.module, function(err) {
-      if (err != null) {
-        bot.say(res.channel, err);
-      }
-      return bot.say(res.channel, "Module '" + res.params.module + "' stopped!");
-    });
-  });
-};
 
 module.exports = (function(_super) {
   __extends(exports, _super);
 
-  function exports(config) {
-    var module, _i, _len, _ref;
-    this.config = config;
+  function exports() {
     this.stop = __bind(this.stop, this);
     this.load = __bind(this.load, this);
-    this.router = new Router;
-    this.modules = {};
-    this.authedClients = [];
+    var module, _i, _len, _ref;
     this.middlewares = [];
-    this.config = this.config || {};
-    this.use(this.constructRes);
-    registerDefaultRoutes(this);
-    if (this.config.modules != null) {
-      _ref = this.config.modules;
+    this.modules = {};
+    exports.__super__.constructor.apply(this, arguments);
+    this.on('error', function(event) {
+      return this.error(JSON.stringify(event.command.toUpperCase()));
+    });
+    if (this.opt.modules != null) {
+      _ref = this.opt.modules;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         module = _ref[_i];
         this.load(module);
       }
     }
   }
+
+  exports.prototype.connect = function() {
+    this.info("Connecting to " + this.opt.server);
+    exports.__super__.connect.apply(this, arguments);
+    return this.once('registered', function() {
+      var channel;
+      return this.info("Connected to " + this.opt.server, "Current channels: " + (((function() {
+        var _results;
+        _results = [];
+        for (channel in this.chans) {
+          _results.push(channel);
+        }
+        return _results;
+      }).call(this)).join(', ')));
+    });
+  };
 
   exports.prototype.log = function() {
     return console.log.apply(console, arguments);
@@ -118,7 +82,7 @@ module.exports = (function(_super) {
 
   exports.prototype.error = function() {
     var msg;
-    if (this.config.debug) {
+    if (this.opt.debug) {
       return console.error.apply(console, ['Error:'.red].concat(__slice.call((function() {
         var _i, _len, _results;
         _results = [];
@@ -129,29 +93,6 @@ module.exports = (function(_super) {
         return _results;
       }).apply(this, arguments))));
     }
-  };
-
-  exports.prototype.say = function() {
-    var _ref;
-    return (_ref = this.irc).say.apply(_ref, arguments);
-  };
-
-  exports.prototype.join = function(channel, cb) {
-    var _this = this;
-    return this.irc.join(channel, function() {
-      if (cb != null) {
-        return cb.apply(_this, arguments);
-      }
-    });
-  };
-
-  exports.prototype.part = function(channel, cb) {
-    var _this = this;
-    return this.irc.part(channel, function() {
-      if (cb != null) {
-        return cb.apply(_this, arguments);
-      }
-    });
   };
 
   exports.prototype.load = function(mod, cb) {
@@ -196,52 +137,24 @@ module.exports = (function(_super) {
     return typeof cb === "function" ? cb(null) : void 0;
   };
 
-  exports.prototype.connect = function() {
-    var _this = this;
-    this.info("Connecting to server " + this.config.address + ".");
-    this.irc = new irc.Client(this.config.address, this.config.nick, this.config);
-    this.channels = this.irc.chans;
-    this.on('error', this.error);
-    this.on('registered', function() {
-      return this.info("Connected to server " + this.config.address + ".\n\tChannels joined: " + (this.config.channels.join(', ')));
-    });
-    this.on('message', function(nick, channel, msg, res) {
-      return _this.match(msg, res);
-    });
-    return this.irc;
-  };
-
-  exports.prototype.route = function() {
-    var fn, middlewares, path, _i;
-    path = arguments[0], middlewares = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), fn = arguments[_i++];
-    return this.router.addRoute(path, this.wrap(fn, middlewares));
-  };
-
-  exports.prototype.on = function() {
+  exports.prototype.addListener = function() {
     var event, fn, middlewares, _i;
     event = arguments[0], middlewares = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), fn = arguments[_i++];
-    this.irc.addListener(event, this.wrap(fn, middlewares, false));
-    return exports.__super__.on.apply(this, arguments);
+    return exports.__super__.addListener.call(this, event, this.wrap(fn, middlewares));
   };
 
-  exports.prototype.match = function(path, data) {
-    var result;
-    if ((result = this.router.match(path)) == null) {
-      return;
-    }
-    return result.fn.call(this, _.extend(result, data));
+  exports.prototype.once = function() {
+    var event, fn, middlewares, _i;
+    event = arguments[0], middlewares = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), fn = arguments[_i++];
+    return exports.__super__.once.call(this, event, this.wrap(fn, middlewares));
   };
 
-  exports.prototype.wrap = function(fn, middlewares, useRegisted) {
+  exports.prototype.wrap = function(fn, middlewares) {
     var _this = this;
-    if (useRegisted == null) {
-      useRegisted = true;
-    }
     return function() {
-      var args, combinedMiddlewares;
+      var args;
       args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      combinedMiddlewares = useRegisted ? _this.middlewares.concat(middlewares) : middlewares;
-      return _.reduceRight(combinedMiddlewares, function(memo, item) {
+      return _.reduceRight(_this.middlewares.concat(middlewares), function(memo, item) {
         var next;
         next = function() {
           return memo.apply(_this, args);
@@ -258,41 +171,6 @@ module.exports = (function(_super) {
     return (_ref = this.middlewares).push.apply(_ref, arguments);
   };
 
-  exports.prototype.constructRes = function(res, next) {
-    res.channel = res.args[0];
-    res.message = res.args[1];
-    res.username = res.user;
-    res.user = !this.authedClients.hasOwnProperty(res.prefix) ? null : this.authedClients[res.prefix];
-    return next();
-  };
-
-  exports.prototype.authenticate = function(res, next) {
-    var user;
-    if (this.config.users == null) {
-      return this.error("Tried to authenticate. No users configured");
-    }
-    if (this.authedClients.hasOwnProperty(res.prefix)) {
-      return this.say(res.channel, "You are already authed.");
-    }
-    user = res.user = _.findWhere(this.config.users, {
-      username: res.params.username,
-      password: res.params.password
-    });
-    if (user == null) {
-      this.error("User " + res.prefix + " tried to authenticate with bad credentials");
-      return this.say(res.channel, "Authentication failed. Bad credentials.");
-    }
-    this.authedClients[res.prefix] = user;
-    return next();
-  };
-
-  exports.prototype.requiresUser = function(res, next) {
-    if (res.user == null) {
-      return this.error("User " + res.prefix + " tried to use '" + res.route + "' route");
-    }
-    return next();
-  };
-
   return exports;
 
-})(EventEmitter);
+})(irc.Client);
--- src/index.coffee
+++ src/index.coffee
@@ -1,70 +1,34 @@
 fs           = require 'fs'
 irc          = require 'irc'
 colors       = require 'colors'
-Router       = require 'routes'
-EventEmitter = require('events').EventEmitter
 _            = require 'underscore'
-_.str        = require 'underscore.string'
 
-pack = JSON.parse fs.readFileSync "#{__dirname}/package.json"
-
-registerDefaultRoutes = (bot) ->
-  bot.route '!info', (res) ->
-    bot.say res.channel, """
-      h :) v#{pack.version}
-      Current channels: #{(chan for chan of bot.channels).join(', ')}
-      #{pack.repository.url}
-      """
-  bot.route '!auth :username :password', bot.authenticate, (res) ->
-    bot.say res.channel, "You are now authed. Hi #{_.str.capitalize(res.user.username)}!"
-
-  bot.route '!join :channel', bot.requiresUser, (res) ->
-    bot.join res.params.channel
-
-  bot.route '!join :channel :password', bot.requiresUser, (res) ->
-    bot.join res.params.channel + ' ' + res.params.password
-
-  bot.route '!part :channel', bot.requiresUser, (res) ->
-    bot.part res.params.channel
-
-  bot.route '!load :module', bot.requiresUser, (res) ->
-    bot.load res.params.module, (err) ->
-      return bot.say res.channel, err if err?
-      bot.say res.channel, "Module '#{res.params.module}' loaded!"
-
-  bot.route '!stop :module', bot.requiresUser, (res) ->
-    bot.stop res.params.module, (err) ->
-      bot.say res.channel, err if err?
-      bot.say res.channel, "Module '#{res.params.module}' stopped!"
-
-
-class module.exports extends EventEmitter
-  constructor: (@config) ->
-    @router = new Router
-    @modules = {}
-    @authedClients = []
+class module.exports extends irc.Client
+  constructor: ->
     @middlewares = []
-    @config = @config or {}
-    @use @constructRes
+    @modules = {}
 
-    registerDefaultRoutes @
+    super
 
-    @load module for module in @config.modules if @config.modules?
+    @on 'error', (event) -> @error JSON.stringify event.command.toUpperCase()
+
+    @load module for module in @opt.modules if @opt.modules?
+
+  connect: ->
+    @info "Connecting to #{@opt.server}"
+
+    super
+
+    @once 'registered', ->
+      @info "Connected to #{@opt.server}",
+            "Current channels: #{(channel for channel of @chans).join ', '}"
 
   log: -> console.log arguments...
   info: -> console.info 'Info:'.green, (msg.green for msg in arguments)...
   warn: -> console.warn 'Warn:'.yellow, (msg.yellow for msg in arguments)...
-  error: -> console.error 'Error:'.red, (msg.red for msg in arguments)... if @config.debug
 
-  say: -> @irc.say arguments...
-
-  join: (channel, cb) ->
-    @irc.join channel, =>
-      cb.apply @, arguments if cb?
-
-  part: (channel, cb) ->
-    @irc.part channel, =>
-      cb.apply @, arguments if cb?
+  error: ->
+    console.error 'Error:'.red, (msg.red for msg in arguments)... if @opt.debug
 
   load: (mod, cb) =>
     try
@@ -91,7 +55,7 @@ class module.exports extends EventEmitter
 
     module.init?(@)
 
-    cb?(null)
+    cb? null
 
   stop: (mod, cb) =>
     unless @modules.hasOwnProperty mod
@@ -105,78 +69,17 @@ class module.exports extends EventEmitter
 
     @info "Stopped module #{mod}"
 
-    return cb?(null)
+    cb? null
 
-  connect: ->
-    @info "Connecting to server #{@config.address}."
+  addListener: (event, middlewares..., fn) ->
+    super event, @wrap fn, middlewares
 
-    @irc = new irc.Client @config.address, @config.nick, @config
+  once: (event, middlewares..., fn) -> super event, @wrap fn, middlewares
 
-    @channels = @irc.chans
-
-    @on 'error', @error
-
-    @on 'registered', ->
-      @info "Connected to server #{@config.address}.\n\tChannels joined: #{@config.channels.join(', ')}"
-
-    @on 'message', (nick, channel, msg, res) =>
-      @match msg, res
-
-    return @irc
-
-
-  route: (path, middlewares..., fn) ->
-    @router.addRoute path, @wrap(fn, middlewares)
-
-  on: (event, middlewares..., fn) ->
-    @irc.addListener event, @wrap(fn, middlewares, false)
-    super
-
-  match: (path, data) ->
-    return unless (result = @router.match path)?
-    result.fn.call @, _.extend result, data
-
-  wrap: (fn, middlewares, useRegisted = true) -> (args...) =>
-    combinedMiddlewares =
-      if useRegisted
-        @middlewares.concat(middlewares)
-      else
-        middlewares
-
-    _.reduceRight(combinedMiddlewares, (memo, item) =>
+  wrap: (fn, middlewares) -> (args...) =>
+    _.reduceRight(@middlewares.concat(middlewares), (memo, item) =>
       next = => memo.apply @, args
       return -> item.apply @, [args..., next]
     , fn).apply @, arguments
 
   use: -> @middlewares.push arguments...
-
-  constructRes: (res, next) ->
-    res.channel = res.args[0]
-    res.message = res.args[1]
-    res.username = res.user
-
-    res.user = unless @authedClients.hasOwnProperty(res.prefix)
-      null
-    else
-      @authedClients[res.prefix]
-
-    next()
-
-  authenticate: (res, next) ->
-    return @error "Tried to authenticate. No users configured" unless @config.users?
-    return @say res.channel, "You are already authed." if @authedClients.hasOwnProperty res.prefix
-
-    user = res.user = _.findWhere(@config.users, username: res.params.username, password: res.params.password)
-
-    unless user?
-      @error "User #{res.prefix} tried to authenticate with bad credentials"
-      return @say res.channel, "Authentication failed. Bad credentials."
-
-    @authedClients[res.prefix] = user
-
-    next()
-
-  requiresUser: (res, next) ->
-    unless res.user?
-      return @error "User #{res.prefix} tried to use '#{res.route}' route"
-    next()
